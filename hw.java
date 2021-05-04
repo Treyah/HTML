548dd52e8fadb7018a932a019fd0c94af6f4c16d
Treyah <73278462+Treyah@users.noreply.github.com>
2021-05-04 03:28:49 -0600

    lmao just some small changes

--- hw.java
+++ hw.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class hw {
     public static void main(String[] args) {
         Scanner joe = new Scanner(System.in);
+    
+        System.out.println("Hello World...");
         String iS = joe.nextLine();
-        scan.close();
+        System.out.println(iS); //iS stands for input string
+        joe.close();
         // scan.close() is a built in method that will close the scanner. Use after you are done gettin the user input from the scanner
-        
+        // bro I'm not even gonna cap... I lowkey forgot how to use java... imma really need to review.. it should come back to me <3 
     }
 }
