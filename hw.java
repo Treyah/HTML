550bf545ce7c7b51124bda749aff948863a146d2
Treyah <73278462+Treyah@users.noreply.github.com>
2021-05-04 03:20:59 -0600

    this is just a beginner java challenge i'm doing from hackerrank lol <3

---
+++ hw.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class hw {
+    public static void main(String[] args) {
+        Scanner joe = new Scanner(System.in);
+        String iS = joe.nextLine();
+        scan.close();
+        // scan.close() is a built in method that will close the scanner. Use after you are done gettin the user input from the scanner
+        
+    }
+}
